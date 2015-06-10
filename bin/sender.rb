ec5c365c9bd2ac2b9c30d9d17e8384c7b60a61d7
James C Russell <james@101ideas.cz>
2015-06-10 15:58:25 +0100

    Time out.

--- bin/sender.rb
+++ bin/sender.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'pony'
+require 'timeout'
+
+begin
+  body = Timeout.timeout(60) { STDIN.read }
+rescue Timeout::Error
+  abort "! Generating the digest timed out!"
+end
 
 Pony.mail({
   to: 'contracts@101ideas.cz',
   subject: 'Contract digest',
   from: 'james@botanicus.me',
-  html_body: STDIN.read,
+  html_body: body,
   via: :smtp,
   # Provided by EasySMTP.
   via_options: {
