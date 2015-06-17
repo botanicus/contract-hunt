0697020f4e76db94e388e95af5bf29d689dc1305
James C Russell <james@101ideas.cz>
2015-06-17 12:55:23 +0200

    No bloody PHP.

--- lib/contract-hunt.rb
+++ lib/contract-hunt.rb
@@ -18,7 +18,7 @@ class Item
   end
 
   def valid?
-    self.to_html.match(/ruby|rails|javascript|angular/i) && ! self.title.match(/devop|java\b/i)
+    self.to_html.match(/ruby|rails|javascript|angular/i) && ! self.title.match(/devop|java\b|php/i)
   end
 
   def to_html
